2d311d9421de366f78577bfa5333619f48a9d1c5
Sergey Kirichenko <spkirich@gmail.com>
2022-11-22 23:43:15 +0300

    Fix code style

--- init.lua
+++ init.lua
@@ -1,131 +1,22 @@
--- Set update time
-vim.opt.updatetime = 150
-
--- Set tab stop
-vim.opt.tabstop = 4
-
--- Set shift width
-vim.opt.shiftwidth = 4
-
--- Enable tab expansion
-vim.opt.expandtab = true
-
--- Enable smart indents
-vim.opt.smartindent = true
-
--- Enable smart tabs
-vim.opt.smarttab = true
-
--- Enable sign column
-vim.opt.signcolumn = "yes"
+-- * Plugins
 
 local packer = require("packer")
 
 packer.startup(function(use)
 
-    -- Plugin manager
+    -- | Use the Plugin Manager
     use "wbthomason/packer.nvim"
 
-    -- Completion engine
-    use {"neoclide/coc.nvim", branch = "release"}
+    -- | Use the Completion Engine
+    use { "neoclide/coc.nvim", branch = "release" }
 
-    -- Color scheme
+    -- | Use the Color Scheme
     use "folke/tokyonight.nvim"
 
-    end
+end
 )
 
--- Diagnostic
-
-vim.api.nvim_set_keymap(
-    "n", "[g", "<Plug>(coc-diagnostics-prev)", {
-        silent = true
-    }
-)
-
-vim.api.nvim_set_keymap(
-    "n", "]g", "<Plug>(coc-diagnostics-next)", {
-        silent = true
-    }
-)
-
--- Navigation
-
-vim.api.nvim_set_keymap(
-    "n", "gd", "<Plug>(coc-definition)", {
-        silent = true
-    }
-)
-
-vim.api.nvim_set_keymap(
-    "n", "gy", "<Plug>(coc-type-definition)", {
-        silent = true
-    }
-)
-
-vim.api.nvim_set_keymap(
-    "n", "gi", "<Plug>(coc-implementation)", {
-        silent = true
-    }
-)
-
-vim.api.nvim_set_keymap(
-    "n", "gr", "<Plug>(coc-references)", {
-        silent = true
-    }
-)
-
--- Actions
-
-vim.api.nvim_set_keymap(
-    "n", "<leader>rn", "<Plug>(coc-rename)", {
-        silent = true
-    }
-)
-
-vim.api.nvim_set_keymap(
-    "n", "<leader>rf", "<Plug>(coc-refactor)", {
-        silent = true
-    }
-)
-
-vim.api.nvim_set_keymap(
-    "n", "<leader>qf", "<Plug>(coc-fix-current)", {
-        silent = true
-    }
-)
-
-vim.api.nvim_set_keymap(
-    "n", "<leader>al", "<Plug>(coc-codeaction-line)", {
-        silent = true
-    }
-)
-
-vim.api.nvim_set_keymap(
-    "n", "<leader>ac", "<Plug>(coc-codeaction-cursor)", {
-        silent = true
-    }
-)
-
-vim.api.nvim_set_keymap(
-    "n", "<leader>cl", "<Plug>(coc-codelens-action)", {
-        silent = true
-    }
-)
-
--- Tools
-
-vim.api.nvim_set_keymap(
-    "n", "<leader>e", "<Cmd>CocCommand explorer<CR>", {
-        silent = true
-    }
-)
-
-vim.api.nvim_set_keymap(
-    "n", "<leader>f", "<Cmd>call CocActionAsync('format')<CR>", {
-        silent = true
-    }
-)
+-- * Appearance
 
 local tokyonight = require("tokyonight")
 
@@ -133,5 +24,111 @@ tokyonight.setup {
     transparent = true
 }
 
--- Set Tokyo Night color scheme
+-- | Set the Color Scheme
 vim.cmd("colorscheme tokyonight")
+
+-- | Enable Sign Column
+vim.opt.signcolumn = "yes"
+
+-- * Code Actions
+
+vim.api.nvim_set_keymap("n", "<leader>rn",
+    "<Plug>(coc-rename)",
+    { silent = true }
+)
+
+vim.api.nvim_set_keymap("n", "<leader>rf",
+    "<Plug>(coc-refactor)",
+    { silent = true }
+)
+
+vim.api.nvim_set_keymap("n", "<leader>qf",
+    "<Plug>(coc-fix-current)",
+    { silent = true }
+)
+
+vim.api.nvim_set_keymap("n", "<leader>al",
+    "<Plug>(coc-codeaction-line)",
+    { silent = true }
+)
+
+vim.api.nvim_set_keymap("n", "<leader>ac",
+    "<Plug>(coc-codeaction-cursor)",
+    { silent = true }
+)
+
+vim.api.nvim_set_keymap("n", "<leader>cl",
+    "<Plug>(coc-codelens-action)",
+    { silent = true }
+)
+
+-- * Explorer
+
+vim.api.nvim_set_keymap("n", "<leader>e",
+    "<Cmd>CocCommand explorer<CR>",
+    { silent = true }
+)
+
+local explorer_group = vim.api.nvim_create_augroup("Explorer",
+    { clear = true }
+)
+
+vim.api.nvim_create_autocmd("FileType",
+    {
+        group = explorer_group,
+
+        command = [[
+
+if &filetype == 'coc-explorer'
+    setlocal statusline=coc-explorer
+endif
+
+]]
+    }
+)
+
+-- * Format
+
+vim.api.nvim_set_keymap("n", "<leader>f",
+    "<Cmd>call CocActionAsync('format')<CR>",
+    { silent = true }
+)
+
+-- * Go to
+
+vim.api.nvim_set_keymap("n", "gd",
+    "<Plug>(coc-definition)",
+    { silent = true }
+)
+
+vim.api.nvim_set_keymap("n", "gy",
+    "<Plug>(coc-type-definition)",
+    { silent = true }
+)
+
+vim.api.nvim_set_keymap("n", "gi",
+    "<Plug>(coc-implementation)",
+    { silent = true }
+)
+
+vim.api.nvim_set_keymap("n", "gr",
+    "<Plug>(coc-references)",
+    { silent = true }
+)
+
+-- * Tabs and Indents
+
+-- | Set tab stop
+vim.opt.tabstop = 4
+
+-- | Set shift width
+vim.opt.shiftwidth = 4
+
+-- | Enable expansion
+vim.opt.expandtab = true
+
+-- | Enable smart indents
+vim.opt.smartindent = true
+
+-- | Enable smart tabs
+vim.opt.smarttab = true
